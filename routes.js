debf0360e4fa35531a587752180cf918df151862
Justin Boundy <jboundy@ISF.COM>
2020-05-21 19:34:50 -0400

    Started api crud endpoints

---
+++ routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/food/previous', (request, response) => {
+    
+});
+
+router.get('/api/food', (request, response) => {
+
+});
+
+router.post('/api/food', (request, response) =>{
+
+});
+
+router.post('/api/food/food-to-calories', (request, response) => {
+
+});
+
+router.put('/api/calorie/:id', (request, response) => {
+
+});
+
+router.put('/api/calorie/goals', (request, response) => {
+
+});
+
+router.get('/api/calorie/track/:goalId', (reauest, response) => {
+
+});
+
+modules.export = router;
--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-'use strict';
-
 const express = require('express');
+const routes = require('/routes');
+const server = express();
 
-// Constants
-const PORT = 9090;
+const PORT = process.env.PORT || 9090;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+server.use(routes);
+server.listen(PORT, HOST);
