246f93e6bb3effe690ec1f3af670ca6fbbe3e9b8
Michael Sanders <mesanders@spotify.com>
2020-06-19 17:24:59 -0400

    Updated the API to read. Crated two scripts to start and kill / rm local redis clusters. For now this is for development purposes. Will need to do something fancier so that we can just pause the redis cluster without losing the data.

--- database.js
+++ database.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const csv = require('csv-parser');  
 const path = require('path');
 const redis = require('redis');
-const client = redis.createClient(6379,'redis');;
+const client = redis.createClient(7001,'localhost');;
 //var Redis = require("ioredis");
 //var redis = new Redis();
+
+
 
 client.on('connect', function() {
     console.log('Redis client connected');
@@ -12,16 +14,7 @@ client.on('connect', function() {
 
 client.on('error', function (err) {
     console.log('Something went wrong ' + err);
- });
-
- fs.createReadStream(path.join(__dirname, 'data/nutrition.csv'))  
-    .pipe(csv())
-    .on('data', (row) => {
-        client.set('docid:', JSON.stringify(row));
-    })
-    .on('end', () => {
-        client.end(false);
-    }); 
+});
 
 function GetAll(){
     return client.get('0');
